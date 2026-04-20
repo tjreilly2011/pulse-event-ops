accf7fe91af20319f02e83e32b110ba671cacfe5
Tom Reilly <tomasreilly@gmail.com>
2026-04-20 22:06:53 +0100

    feat(BE-02): add SseEvent domain type

--- src/domain/event_update.rs
+++ src/domain/event_update.rs
@@ -2,7 +2,7 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, sqlx::FromRow)]
+#[derive(Debug, Clone, Serialize, sqlx::FromRow)]
 pub struct EventUpdate {
     pub id: Uuid,
     pub event_id: Uuid,
